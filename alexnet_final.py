dfa59ced50a9b7c13112b1bb2490a256042f2c28
juuhnpark <juuhnpark@gmail.com>
2020-04-22 21:20:16 -0400

    ready

--- alexnet_final.py
+++ alexnet_final.py
@@ -17,12 +17,12 @@ preprocess = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
-for image in os.listdir(path):
-    try:
-        input_image = Image.open(path+'/'+image)
-        images.append(preprocess(input_image).unsqueeze(0))
-    except:
-        continue
+# for image in os.listdir(path):
+#     try:
+#         input_image = Image.open(path+'/'+image)
+#         images.append(preprocess(input_image).unsqueeze(0))
+#     except:
+#         continue
 
 alexnet_model = models.alexnet(pretrained=True)
 
@@ -49,19 +49,28 @@ class SingleLayer(nn.Module):
 
 if __name__ == "__main__":
 
-    prev_output = images
+    # prev_output = images
 
-    for layer in range(0,22):
-        print("LAYER : ", layer, all_layers[layer])
-        current_layer = SingleLayer(layer)
+    layers = [SingleLayer(i) for i in range(0, 22)]
+    time = [0. for i in range(0,22)]
 
-        start = timer()
+    for j, image in enumerate(os.listdir(path)):
+        curr_image = preprocess(Image.open(path+'/'+image)).unsqueeze(0)
+        time_taken = 0.0
+        for i, layer in enumerate(layers):
+            start = timer()
 
-        for i, image in enumerate(prev_output):
-            prev_output[i] = current_layer(image)
+            curr_image = layer(curr_image)
 
-        end = timer()
-        
-        torch.save(prev_output, 'tensor' + str(layer) + '.pt')
-        print('\nTime Taken:', (end-start)/9811.)
-        print('------------------------------')
+            end = timer()
+
+            time[i] += (end-start)
+            time_taken += (end-start)
+
+        print(j, time_taken)
+
+    np.divide(time, 9812)
+
+    for i, t in enumerate(time):
+        print(layers[i])
+        print('Layer', i, t)
