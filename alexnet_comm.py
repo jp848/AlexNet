67765c6b4f771e4e0533f6d80fae8d6be8112588
juuhnpark <juuhnpark@gmail.com>
2020-05-09 19:42:24 -0400

    f

--- alexnet_comm.py
+++ alexnet_comm.py
@@ -9,13 +9,13 @@ from PIL import Image
 import argparse
 import socket
 import pickle
-import time
 from functools import reduce
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-cloud', action='store_true', default=False,
                     dest='cloud')
-parser.add_argument('layer')
+# parser.add_argument('layer')
 
 images = []
 path = os.curdir + '/images'
@@ -53,54 +53,54 @@ class SingleLayer(nn.Module):
 if __name__ == "__main__":
 
     args = parser.parse_args()
-    is_cloud, layer = args.cloud, int(args.layer)
+    is_cloud = args.cloud
     layers = [SingleLayer(i) for i in range(0, 22)]
 
     if not is_cloud:
         TCP_IP1 = '10.0.2.2'
-        # TCP_IP1 = '127.0.0.1'
         TCP_PORT = 8080
         
         process_time = 0.0
         comm_time = 0.0
+        for layer in range(0,22):
+            for i, image in enumerate(os.listdir(path)):
+                input_image = preprocess(Image.open(path+'/'+image)).unsqueeze(0)
 
-        for i, image in enumerate(os.listdir(path)):
-            input_image = preprocess(Image.open(path+'/'+image)).unsqueeze(0)
+                process_start = timer()
 
-            process_start = timer()
+                output = reduce(lambda o, func: func(o), layers[:layer], input_image)
 
-            output = reduce(lambda o, func: func(o), layers[:layer], input_image)
+                process_end = timer()
 
-            process_end = timer()
+                #send to server
+                comm_start = timer()
 
-            #send to server
-            comm_start = timer()
+                s=socket.socket()
+                s.connect((TCP_IP1,TCP_PORT))
+                send_x=output.detach().numpy()
+                data_input=pickle.dumps(send_x, protocol=pickle.HIGHEST_PROTOCOL)
+                s.sendall(data_input)
+                s.close()
+                
+                comm_end = timer()
 
-            s=socket.socket()
-            s.connect((TCP_IP1,TCP_PORT))
-            send_x=output.detach().numpy()
-            data_input=pickle.dumps(send_x, protocol=pickle.HIGHEST_PROTOCOL)
-            s.sendall(data_input)
-            s.close()
-            
-            comm_end = timer()
+                process_time += (process_end - process_start)
+                comm_time += (comm_end - comm_start)
 
-            process_time += (process_end - process_start)
-            comm_time += (comm_end - comm_start)
+                # print('Image #' + str(i) + ' sent.')
 
-            print('Image #' + str(i) + ' sent.')
-
-        print("Avg. Process Time:", process_time/len(os.listdir(path)))
-        print("Avg. Comm Time:", comm_time/len(os.listdir(path)))
+            print(layer, "Avg. Process Time:", process_time/len(os.listdir(path)))
+            print(layer, "Avg. Comm Time:", comm_time/len(os.listdir(path)))
 
     else:
-        TCP_IP1 = '127.0.0.1'
         TCP_IP2 = '127.0.0.1'
         TCP_PORT = 8080
         s=socket.socket()
-
+        
         s.bind((TCP_IP2, TCP_PORT))
         cnt, process_time = 0, 0.0
+        layer = 0
+        
         while 1:
             cnt += 1
             s.listen(1)
@@ -125,5 +125,7 @@ if __name__ == "__main__":
 
             process_time += (process_end - process_start)
 
-            if cnt == len(os.listdir(path)):
+            if cnt % len(os.listdir(path)) == 0:
                 print("Avg. Process Time:", process_time/cnt)
+                os.system('say "your program has finished"')
+                layer += 1
